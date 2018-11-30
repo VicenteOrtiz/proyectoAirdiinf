ca93e6c21d5d0b1192334de771b48872b1bf87cf
Vicente <Vicente@MacBook-Pro-de-Vicente.local>
2018-11-30 18:26:28 -0300

    cree la tabla paquete-reserva

---
+++ database/migrations/2018_11_30_211543_create_packagereserves_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePackagereservesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('packagereserves', function (Blueprint $table) {
+            // $table->increments('id');
+            // $table->timestamps();
+
+            $table->integer('reserve_id');
+            $table->integer('package_id');
+
+            $table->foreign('reserve_id')->references('id')->on('reserves');
+            $table->foreign('package_id')->references('id')->on('packages');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('packagereserves');
+    }
+}
