51aaaac3025afa388d6ceed6ac866c55b1ad5fc4
Vicente Vega <vicente.vega@usach.cl>
2018-12-01 20:47:03 -0300

    se agrega tabla flightsegments

---
+++ database/migrations/2018_12_01_231556_create_flightsegments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFlightsegmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('flightsegments', function (Blueprint $table) {
+
+            $table->integer('flight_id');
+            $table->integer('segment_id');
+
+            $table->foreign('flight_id')->references('id')->on('flights');
+            $table->foreign('segment_id')->references('id')->on('segments');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('flightsegments');
+    }
+}
