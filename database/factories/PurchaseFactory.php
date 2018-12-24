29bc0b7f884b49697b003b2cf984d05e440596a4
Francisco <francisco.diaz.s@usach.cl>
2018-12-24 15:16:32 -0300

    Agregado seeder de Purchase

--- database/factories/PurchaseFactory.php
+++ database/factories/PurchaseFactory.php
@@ -3,7 +3,10 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Purchase::class, function (Faker $faker) {
+	$payment_id = DB::table('paymets')->select('id')->get();
+    $reserve_id = DB::table('reserves')->select('id')->get();
     return [
-        //
+        'totalPrice' => $faker->$faker->numberBetween(100,20000),
+    	'date'=> $faker->dateTime('now',null),
     ];
 });
